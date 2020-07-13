fc091f3b73e63f15ea218534b3d23254a75fc602
Justin Majetich <justinmajetich@gmail.com>
2020-07-13 15:57:24 -0400

    Implement locale-specific formatting

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: " + percent.ToString("P2"));
+		Console.WriteLine("Currency: " + currency.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-US.utf-8")));
+	}
+}
