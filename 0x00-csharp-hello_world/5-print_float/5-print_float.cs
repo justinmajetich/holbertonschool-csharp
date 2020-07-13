3588d0387bff67986c36470356098f2950672b25
Justin Majetich <justinmajetich@gmail.com>
2020-07-13 15:27:05 -0400

    Change formatting method

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-            Console.WriteLine($"Float: {number.ToString("0.00")}");
+            Console.WriteLine("Float: " + number.ToString("F2"));
         }
 }
