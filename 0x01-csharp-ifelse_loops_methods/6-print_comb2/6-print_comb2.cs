ca1c050acf73b82c8e494d0ce3fefa53f45406ff
justinmajetich <justinmajetich@gmail.com>
2020-07-14 12:27:32 -0400

    Remove namespace

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 
-namespace _6_print_comb2
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            for (int n1 = 0; n1 <= 9; n1++) {
-                for (int n2 = n1 + 1; n2 <= 9; n2++) {
-                    if (n1 == 8 && n2 ==9)
-                        Console.WriteLine($"{n1}{n2}");
-                    else
-                        Console.Write($"{n1}{n2}, ");
-                }
+        for (int n1 = 0; n1 <= 9; n1++) {
+            for (int n2 = n1 + 1; n2 <= 9; n2++) {
+                if (n1 == 8 && n2 ==9)
+                    Console.WriteLine($"{n1}{n2}");
+                else
+                    Console.Write($"{n1}{n2}, ");
             }
         }
     }
