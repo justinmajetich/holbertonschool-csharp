b7834e2298ee04bbc8526e5e333a59c8f2ba2c63
justinmajetich <justinmajetich@gmail.com>
2020-07-14 15:31:22 -0400

    Print distinct messages according to number in range

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 100; i++) {
+                if (i % 15 == 0) {
+                    Console.Write("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    Console.Write("Buzz");
+                } else if (i % 3 == 0) {
+                    Console.Write("Fizz");
+                } else {
+                    Console.Write(i);
+                }
+
+                if (i != 100) {
+                    Console.Write(" ");
+                }
+            }
+
+        }
+    }
+}
