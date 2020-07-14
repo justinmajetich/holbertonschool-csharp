9871788de7e281262b92fe877d27656bbb8cc063
justinmajetich <justinmajetich@gmail.com>
2020-07-14 11:54:33 -0400

    Print alphabet excluding certain characters

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'a'; c <= 'z'; c++) {
+                if (c != 'q' && c != 'e')
+                    Console.Write(c);
+            }
+        }
+    }
+}
