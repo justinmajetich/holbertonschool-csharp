478a1b695eb214eda421abc4e7fc659c7b4b2777
justinmajetich <justinmajetich@gmail.com>
2020-07-14 15:10:15 -0400

    Print diagonal line

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintDiagonal(int length) {
+        for (var y = 0; y < length; y++) {
+            for (var x = 0; x < y; x++) {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.WriteLine();
+    }
+}
