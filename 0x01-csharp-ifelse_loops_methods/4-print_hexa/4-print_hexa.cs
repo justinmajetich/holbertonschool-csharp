bc7ba687809ac56d2457b431589b940d6e1c4d18
justinmajetich <justinmajetich@gmail.com>
2020-07-14 13:00:30 -0400

    Change format specifier to print lowercase hex

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,7 +7,7 @@ namespace _4_print_hexa
         static void Main(string[] args)
         {
             for (int n = 0; n < 99; n++)
-                Console.WriteLine($"{n} = 0x{n.ToString("X")}");
+                Console.WriteLine($"{n} = 0x{n.ToString("x")}");
         }
     }
 }
