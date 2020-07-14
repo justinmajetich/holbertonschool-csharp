cb6dc316194acc70d88c3711af30494eeb795c04
justinmajetich <justinmajetich@gmail.com>
2020-07-14 11:57:57 -0400

    Print range in hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n < 99; n++)
+                Console.WriteLine($"{n} = 0x{n.ToString("X")}");
+        }
+    }
+}
