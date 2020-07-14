d3d9bb08df7c90ad1ff7c7bb9498296834c4dcae
justinmajetich <justinmajetich@gmail.com>
2020-07-14 11:47:22 -0400

    Print alphabet in lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i < 123; i++)
+            Console.Write((char)i);
+        }
+    }
+}
