911ceab50e56e500a6dd6683cbb81775c5a317f1
justinmajetich <justinmajetich@gmail.com>
2020-07-14 12:03:51 -0400

    Print formatted range of numbers

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n < 100; n++) {
+                if (n != 99)
+                    Console.Write(n.ToString("D2") + ", ");
+                else
+                    Console.Write(n.ToString("D2") + "\n");
+            }
+        }
+    }
+}
