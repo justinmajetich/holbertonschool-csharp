faaf35776b40d4c05a5844372c3e0b342ae95d6a
justinmajetich <justinmajetich@gmail.com>
2020-07-14 11:40:37 -0400

    Determine category of random number

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+
+        if (lastDigit > 5)
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is greater than 5");
+        else if (lastDigit == 0)
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is 0");
+        else
+            Console.WriteLine($"The last digit of {number} is {lastDigit} and is less than 6 and not 0");
+    }
+}
