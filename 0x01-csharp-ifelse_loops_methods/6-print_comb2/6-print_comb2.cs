7f73ecba32bf260323c1bc06fc94a6f2992e70b2
justinmajetich <justinmajetich@gmail.com>
2020-07-14 12:22:54 -0400

    Print range of numbers without digit repetition

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n1 = 0; n1 <= 9; n1++) {
+                for (int n2 = n1 + 1; n2 <= 9; n2++) {
+                    if (n1 == 8 && n2 ==9)
+                        Console.WriteLine($"{n1}{n2}");
+                    else
+                        Console.Write($"{n1}{n2}, ");
+                }
+            }
+        }
+    }
+}
