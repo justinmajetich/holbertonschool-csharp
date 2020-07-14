a047efc8442349aa7f4b758d7b8c2c9a1cf2a757
justinmajetich <justinmajetich@gmail.com>
2020-07-14 14:58:59 -0400

    Print line the length of user arg

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length) {
+        if (length >= 0) {
+            for (var i = 0; i < length; i++) {
+                Console.Write("_");
+            }
+        }
+        Console.Write("\n");
+    }
+}
