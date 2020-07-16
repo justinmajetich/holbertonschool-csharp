53814026cfbda98b9da2df6947203ef20ee22caa
justinmajetich <justinmajetich@gmail.com>
2020-07-16 15:22:26 -0400

    Convert int list to bool list

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> newList = new List<bool>();
+        myList.ForEach(number => newList.Add((number % 2 == 0) ? true : false));
+        return newList;
+    }
+}
