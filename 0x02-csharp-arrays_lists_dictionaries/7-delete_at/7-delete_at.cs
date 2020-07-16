0fbe40e8e0861fc35be01d720788465a508f07fb
justinmajetich <justinmajetich@gmail.com>
2020-07-16 16:07:44 -0400

    Remove element by index

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index > myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+        }
+        else
+        {
+            for (var i = index; i < myList.Count; i++)
+            {
+                if (i + 1 < myList.Count)
+                {
+                    myList[i] = myList[i + 1];
+                }
+            }
+            myList.RemoveRange(myList.Count - 1, 1);
+        }
+        return myList;
+    }
+}
