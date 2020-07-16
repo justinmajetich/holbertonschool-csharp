3646498f6b3d18961eb7f53fe4a6d947790c8732
justinmajetich <justinmajetich@gmail.com>
2020-07-16 16:37:51 -0400

    Create new dictionary from manipulated values

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int> entry in myDict)
+        {
+            newDict[entry.Key] = entry.Value * 2;
+        }
+        return newDict;
+    }
+}
