6e2d60b83b04f55d69b77b237ad17e488cf6d7b5
justinmajetich <justinmajetich@gmail.com>
2020-07-16 17:14:08 -0400

    Determine best score in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        KeyValuePair<string, int> bestScore = new KeyValuePair<string, int>("None", 0);
+
+        foreach (KeyValuePair<string, int> kv in myList)
+        {
+            if (bestScore.Key == "None" || bestScore.Value < kv.Value)
+            {
+                bestScore = kv;
+            }
+        }
+
+        return bestScore.Key;
+    }
+}
