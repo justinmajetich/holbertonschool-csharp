c8de1495190e6441cddd727db84d9e2ec589d0ac
justinmajetich <justinmajetich@gmail.com>
2020-07-16 15:05:36 -0400

    Create new list

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = new List<int>();
+
+        for (var i = 0; i < size; i++)
+        {
+            Console.Write(i);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+            newList.Add(i);
+        }
+        Console.WriteLine();
+        
+        return newList;
+    }
+}
