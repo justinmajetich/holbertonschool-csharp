3fe57d946eaed411546c527fabf63214e2e0658b
justinmajetich <justinmajetich@gmail.com>
2020-07-16 13:30:39 -0400

    Replace value at index in array

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            array[index] = n;
+        }
+        else
+        {
+            Console.WriteLine("Index out of range");
+        }
+        return array;
+
+    }
+}
