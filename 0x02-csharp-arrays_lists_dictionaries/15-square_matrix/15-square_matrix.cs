b91105c452e779d81a8170937d8a769307b71a84
justinmajetich <justinmajetich@gmail.com>
2020-07-16 17:40:48 -0400

    Square matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int yLength = myMatrix.GetLength(0);
+        int xLength = myMatrix.GetLength(0);
+        
+        int[,] newMatrix = new int[yLength,xLength];
+
+        for (var y = 0; y < yLength; y++)
+        {
+            for (var x = 0; x < xLength; x++)
+            {
+                newMatrix[y,x] = (myMatrix[y,x] * myMatrix[y,x]);
+            }
+        }
+        return newMatrix;
+    }
+}
