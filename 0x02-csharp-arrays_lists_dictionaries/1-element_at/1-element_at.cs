ae78321bf7cb0e94b0e48b85f8a6f0a15f5b1b31
justinmajetich <justinmajetich@gmail.com>
2020-07-16 13:09:35 -0400

    Return value at given index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            return array[index];
+        }
+        else
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+    }
+}
