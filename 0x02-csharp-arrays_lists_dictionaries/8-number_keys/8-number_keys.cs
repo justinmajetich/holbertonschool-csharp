1a4d0742a0cec6996c302c919a4c4c227c2e9bde
justinmajetich <justinmajetich@gmail.com>
2020-07-16 16:24:10 -0400

    Count keys in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        var counter = 0;
+
+        foreach (KeyValuePair<string, string> entry in myDict)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+}
