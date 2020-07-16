0faf8b2f84233c2f163f396155ea6f5d257d7f96
justinmajetich <justinmajetich@gmail.com>
2020-07-16 12:53:47 -0400

    Create and print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] arr = new int[size];
+
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            Console.Write(i);
+            if (i < arr.Length - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return arr;
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,2 @@
+# 0x02. C# - Data Structures: Arrays, Lists, Dictionaries
+This directory contains answer files to a Holberton School project on the basics of Collections in C#.
