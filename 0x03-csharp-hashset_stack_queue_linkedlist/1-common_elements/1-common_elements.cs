3885bbfeb5bc35b2915af5553623aa0621043f01
justinmajetich <justinmajetich@gmail.com>
2020-07-27 12:34:45 -0400

    Create sorted list from common elements of two lists

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        // Create a SortedSet from List
+        var sortedSet = new SortedSet<int>(list1);
+
+        // Intersect sorted set with second collection
+        sortedSet.IntersectWith(list2);
+
+        // Convert SortedSet back to List and return
+        return new List<int>(sortedSet);
+    }
+}
