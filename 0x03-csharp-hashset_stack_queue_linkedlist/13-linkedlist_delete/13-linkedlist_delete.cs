860974627e0649419a7cfedf01a2dec29d543f92
justinmajetich <justinmajetich@gmail.com>
2020-07-27 18:18:08 -0400

    Remove node from linked list by index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int i = 0;
+
+        while (current != null)
+        {
+            if (i == index)
+            {
+                myLList.Remove(current);
+                break;
+            }
+            current = current.Next;
+            i++;
+        }
+    }
+}
