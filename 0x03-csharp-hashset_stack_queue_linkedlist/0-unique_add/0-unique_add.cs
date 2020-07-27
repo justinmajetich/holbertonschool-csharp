1a01052de273e4d8066f5ab2de1a7ea9e6c515f3
justinmajetich <justinmajetich@gmail.com>
2020-07-27 11:59:08 -0400

    Remove duplicates from List by initializing as HashSet

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        // Convert List to HashSet, automatically removing duplicates
+        HashSet<int> hashSet = new HashSet<int>(myList);
+
+        // Return sum of HashSet using Linq Sum() method
+        return hashSet.Sum();
+    }
+}
