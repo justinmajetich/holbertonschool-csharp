5a28dfc8c6fd31d96ea734648e11b2a9189e232c
justinmajetich <justinmajetich@gmail.com>
2020-07-27 14:42:03 -0400

    Manipulate stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        // Take size of stack
+        var count = aStack.Count;
+        Console.WriteLine("Number of items: {0}", count);
+
+        // Peek top value if stack is not empty
+        if (count > 0)
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+         
+        // Determine if stack contains value
+        var containsSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+
+        // If search value present, remove items up to and including value 
+        if (containsSearch)
+        {
+            while (true)
+            {
+                var item = aStack.Peek();
+                aStack.Pop();
+                if (item == search)
+                {
+                    break;
+                }
+            }
+        }
+
+         // Add new item to stack
+        aStack.Push(newItem);
+        
+        return aStack;
+    }
+}
