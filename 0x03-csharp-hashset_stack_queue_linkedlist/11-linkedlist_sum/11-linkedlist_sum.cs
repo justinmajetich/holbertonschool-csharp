61c86898d92e5d9d760e80cb040c8d69870b1661
justinmajetich <justinmajetich@gmail.com>
2020-07-27 18:01:08 -0400

    Sum values in linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        var sum = 0;
+
+        foreach (var node in myLList)
+        {
+            sum += node;
+        }
+        return sum;
+    }
+}
