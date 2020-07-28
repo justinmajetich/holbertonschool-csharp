1d9a396dea008d773f73136ccf98b3902603a4e4
justinmajetich <justinmajetich@gmail.com>
2020-07-28 08:38:46 -0400

    Handle division by zero exception

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int quotient = 0;
+
+        try
+        {
+            quotient = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            quotient = 0;
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {quotient}");
+        }
+    }
+}
