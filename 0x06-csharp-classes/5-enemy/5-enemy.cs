23387b8c7099688760a569872b1877f3daf2d9ef
justinmajetich <justinmajetich@gmail.com>
2020-07-30 14:47:12 -0400

    Override the ToString method

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents an enemy zombie
+    /// </summary>
+    class Zombie
+    {
+        // A Zombie's current health points
+        private int health;
+        
+        // A Zombie's name
+        private string name = "(No name)";
+
+        /// <summary>Gets and sets a Zombie's name.null</summary>
+        public string Name {
+            get => name;
+            set => name = value;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with health value.
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this.health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+
+        /// <summary>
+        /// Gets the value of the heath property.
+        /// </summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        /// <summary>
+        /// Prints a string representation of the Zombie instance
+        /// </summary>
+        public override string ToString()
+        {
+            return $"Zombie Name: {this.name} / Total Health: {this.health}";
+        }
+    }
+}
