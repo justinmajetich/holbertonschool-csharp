07de4c0adbfff81b528b2449fec0696877c8d413
justinmajetich <justinmajetich@gmail.com>
2020-07-30 14:26:02 -0400

    Add alternative constructor accepting initial health value

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents an enemy zombie
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>Represents the Zombie's health points</summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with health value.
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this.health = 0;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
