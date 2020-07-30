8804024a2c2a90309a360351526269137589c833
justinmajetich <justinmajetich@gmail.com>
2020-07-30 14:21:26 -0400

    Add constructor and health field

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents an enemy zombie
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>Represents the Zombie's health points</summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
