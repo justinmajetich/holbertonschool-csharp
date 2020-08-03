6f5d9c0d42e4f6d3b7eb4a36b27829aca437f2e1
justinmajetich <justinmajetich@gmail.com>
2020-08-03 16:25:35 -0400

    Add negative test values

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -7,20 +7,14 @@ namespace MyMath.Tests
     {
         [Test]
         public void Add_WhenTwoIntsAdded_ReturnsCorrectSum(
-            [Values(1, 3, 5, 10, 18, 28, 56, 199, 1035, 30456)] int a,
-            [Values(0, 6, 9, 13, 17, 42, 78, 852, 9099, 12000)] int b)
+            [Values(1, -3, 5, -10, 18, -28, 56, 199, 1035, 30456)] int a,
+            [Values(0, -6, 9, 13, 17, 42, 78, 852, -9099, 12000)] int b)
         {
             // Act - add two ints using Add method
             var testSum = Operations.Add(a, b);
 
             // Assert - correct sum is produced
             Assert.That(testSum == (a + b));
-        }
-
-        [Test]
-        public void Add_WhenParameterMissing_ReturnsException()
-        {
-            Assert.Pass();
         }
     }
 }
