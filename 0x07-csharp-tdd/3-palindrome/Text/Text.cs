5dc952fb4984e8a8c10e6695ba3b6e6b2e120214
justinmajetich <justinmajetich@gmail.com>
2020-08-04 11:19:39 -0400

    Write tests from a palindrome checking method

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Provides a collection of string utilities.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Check if string argument is a palindrome.
+        /// </summary>
+        /// <param name="s">A string to be tested as a palindrome.</param>
+        /// <returns>Returns true if string is a palindrome; otherwise, returns false.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            // Standardize sting to lowercase
+            var sLower = s.ToLower();
+
+            // Create copy of string in reverse
+            char[] sCharArray = sLower.ToCharArray();
+            Array.Reverse(sCharArray);
+            string sReverse = new string(sCharArray);
+
+            return sLower == sReverse;
+        }
+    }
+}
