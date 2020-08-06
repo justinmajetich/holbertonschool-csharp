c0ff52c1d27007f995d41d5109a45d4daefd09ab
justinmajetich <justinmajetich@gmail.com>
2020-08-06 15:20:23 -0400

    Add message to thrown exception

--- 0x08-csharp-inheritance/8-shape/8-shape.cs
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -13,7 +13,7 @@ class Square : Rectangle
         {
             if (value < 0)
             {
-                throw new ArgumentException("");
+                throw new ArgumentException("Size must be greater than or equal to 0");
             }
             else
             {
