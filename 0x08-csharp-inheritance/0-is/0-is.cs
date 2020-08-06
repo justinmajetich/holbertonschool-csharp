13280a461a5a87011e11d8e9b7d988c2fb5d1977
justinmajetich <justinmajetich@gmail.com>
2020-08-06 12:44:47 -0400

    Check if obj passed is an integer

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Provides type checking behavior.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Determines whether of not an object is an in.
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
