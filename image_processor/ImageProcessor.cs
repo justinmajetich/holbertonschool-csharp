43f01c7e94e85ecdbfd2810ec7f475ce2e7b63a5
justinmajetich <justinmajetich@gmail.com>
2020-08-18 13:04:41 -0400

    Write method to invert a list of images

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace ImageProcessor
-{
-    class Program
-    {
-        static void Main(string[] args)
+
+class ImageProcessor {
+    public static void Inverse(string[] filenames) {
+
+        // Iterate through all .jpg files in images directory
+        foreach (var imagePath in filenames)
         {
-            Console.WriteLine("Hello World!");
+            // For each image file create a new Bitmap object
+            Bitmap image = new Bitmap(imagePath);
+
+            // Create an async task to process each image
+            Task task = Task.Factory.StartNew( () => {
+
+                // Iterate through each pixel, inverting its color
+                for (int y = 0; y < image.Height; y++)
+                {
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        // Get color of current pixel
+                        Color pixelColor = image.GetPixel(x, y);
+
+                        // Set color of current pixel to inverse
+                        image.SetPixel(x, y, Color.FromArgb(pixelColor.A,
+                            255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B));
+                    }
+                }
+            } );
+            // Pause main thread
+            task.Wait();
+
+            // Extract filename from path and edit for new save
+            string[] nameSplit = imagePath.Split(new Char[] {'/', '.'});
+            String newFilename = nameSplit[nameSplit.Length - 2] + "_inverse." +
+                                    nameSplit[nameSplit.Length - 1];
+
+            // Save inverted image to new file
+            image.Save(newFilename);
         }
     }
 }
