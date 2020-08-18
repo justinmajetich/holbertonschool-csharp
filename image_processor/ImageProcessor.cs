ceeb1a47c9ce9ff1f7992dc1def1992cb4b2b182
justinmajetich <justinmajetich@gmail.com>
2020-08-18 13:24:45 -0400

    Change foreach loop for Parallel.ForEach

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -15,38 +15,40 @@ class ImageProcessor {
     public static void Inverse(string[] filenames) {
 
         // Iterate through all .jpg files in images directory
-        foreach (var imagePath in filenames)
+        Parallel.ForEach (filenames, imagePath => 
         {
-            // For each image file create a new Bitmap object
-            Bitmap image = new Bitmap(imagePath);
+            {
+                // For each image file create a new Bitmap object
+                Bitmap image = new Bitmap(imagePath);
 
-            // Create an async task to process each image
-            Task task = Task.Factory.StartNew( () => {
+                // Create an async task to process each image
+                Task task = Task.Factory.StartNew( () => {
 
-                // Iterate through each pixel, inverting its color
-                for (int y = 0; y < image.Height; y++)
-                {
-                    for (int x = 0; x < image.Width; x++)
+                    // Iterate through each pixel, inverting its color
+                    for (int y = 0; y < image.Height; y++)
                     {
-                        // Get color of current pixel
-                        Color pixelColor = image.GetPixel(x, y);
+                        for (int x = 0; x < image.Width; x++)
+                        {
+                            // Get color of current pixel
+                            Color pixelColor = image.GetPixel(x, y);
 
-                        // Set color of current pixel to inverse
-                        image.SetPixel(x, y, Color.FromArgb(pixelColor.A,
-                            255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B));
+                            // Set color of current pixel to inverse
+                            image.SetPixel(x, y, Color.FromArgb(pixelColor.A,
+                                255 - pixelColor.R, 255 - pixelColor.G, 255 - pixelColor.B));
+                        }
                     }
-                }
-            } );
-            // Pause main thread
-            task.Wait();
+                } );
+                // Pause main thread
+                task.Wait();
 
-            // Extract filename from path and edit for new save
-            string[] nameSplit = imagePath.Split(new Char[] {'/', '.'});
-            String newFilename = nameSplit[nameSplit.Length - 2] + "_inverse." +
-                                    nameSplit[nameSplit.Length - 1];
+                // Extract filename from path and edit for new save
+                string[] nameSplit = imagePath.Split(new Char[] {'/', '.'});
+                String newFilename = nameSplit[nameSplit.Length - 2] + "_inverse." +
+                                        nameSplit[nameSplit.Length - 1];
 
-            // Save inverted image to new file
-            image.Save(newFilename);
-        }
+                // Save inverted image to new file
+                image.Save(newFilename);
+            }
+        });
     }
 }
