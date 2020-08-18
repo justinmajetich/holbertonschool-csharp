837cca0bf64612234fe00d69bb5cd46ee0dc4ae4
justinmajetich <justinmajetich@gmail.com>
2020-08-18 15:57:37 -0400

    Implement method to convery image to black and white

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -10,6 +10,66 @@ using System.Collections.Generic;
 class ImageProcessor {
 
     /// <summary>
+    /// Converts a list of image(s) to black and white.
+    /// </summary>
+    /// <param name="filenames">A list of images to invert.</param>
+    /// <param name="threshold">
+    /// A threshold for determining whether a pixel should go black or white.
+    /// </param>
+    public static void BlackWhite(string[] filenames, double threshold)
+    {
+        // Iterate through all .jpg files in images directory
+        Parallel.ForEach(filenames, (imagePath) =>
+        {
+            {
+                // For each image file create a new Bitmap object
+                Bitmap image = new Bitmap(imagePath);
+
+                // Lock the bitmaps bits
+                BitmapData bmpData = image.LockBits(
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    ImageLockMode.ReadWrite, image.PixelFormat);
+
+                // Determine size of image in bytes
+                int bytes = bmpData.Stride * bmpData.Height;
+
+                // Allocate buffer size of image bytes
+                byte[] rgbBuffer = new byte[bytes];
+
+                // Safely copy bitmap data to buffer
+                System.Runtime.InteropServices.Marshal.Copy(bmpData.Scan0, rgbBuffer, 0, bytes);
+
+                // Iterate through RGB buffer, inverting each color value to gray
+                for (var i = 0; i + 2 < bytes; i += 3)
+                {
+                    byte gray = (byte)((0.21 * rgbBuffer[i]) + 
+                                       (0.71 * rgbBuffer[i + 1]) + 
+                                       (0.071 * rgbBuffer[i + 2]));
+                    
+                    // Compare grayscale against threshold to determine black or white
+                    gray = (byte)(gray >= threshold ? 255 : 0);
+
+                    rgbBuffer[i] = rgbBuffer[i + 1] = rgbBuffer[i + 2] = gray;
+                }
+
+                // Copy values back from buffer
+                System.Runtime.InteropServices.Marshal.Copy(rgbBuffer, 0, bmpData.Scan0, bytes);
+
+                // Unlock bits
+                image.UnlockBits(bmpData);
+
+                // Extract filename from path and edit for new save
+                string[] nameSplit = imagePath.Split(new Char[] {'/', '.'});
+                String newFilename = nameSplit[nameSplit.Length - 2] + "_bw." +
+                                        nameSplit[nameSplit.Length - 1];
+
+                // Save inverted image to new file
+                image.Save(newFilename);
+            }
+        });
+    }
+
+    /// <summary>
     /// Converts a list of image(s) to grayscale.
     /// </summary>
     /// <param name="filenames">A list of images to invert.</param>
