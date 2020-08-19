f764bd4443ad365bdacba28b4931551d9ef6f494
justinmajetich <justinmajetich@gmail.com>
2020-08-19 13:03:56 -0400

    Implement method to convert image to thumbnail

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -10,6 +10,36 @@ using System.Collections.Generic;
 class ImageProcessor {
 
     /// <summary>
+    /// Converts a list of image(s) to thumbnails of given height. 
+    /// </summary>
+    /// <param name="filenames">List of images to convert.</param>
+    /// <param name="height">Target height of thumbnail.</param>
+    public static void Thumbnail(string[] filenames, int height)
+    {
+        // Iterate through all .jpg files in images directory
+        Parallel.ForEach(filenames, (imagePath) =>
+        {
+            {
+                // For each image file create a new Bitmap object
+                Bitmap image = new Bitmap(imagePath);
+
+                // Calculate new thumbnail width
+                int thumbnailWidth = image.Width / (image.Height / height);
+
+                Image thumbnail = image.GetThumbnailImage(thumbnailWidth, height, () => { return false; }, IntPtr.Zero);
+
+                // Extract filename from path and edit for new save
+                string[] nameSplit = imagePath.Split(new Char[] {'/', '.'});
+                String newFilename = nameSplit[nameSplit.Length - 2] + "_th." +
+                                        nameSplit[nameSplit.Length - 1];
+
+                // Save inverted image to new file
+                thumbnail.Save(newFilename);
+            }
+        });
+    }
+
+    /// <summary>
     /// Converts a list of image(s) to black and white.
     /// </summary>
     /// <param name="filenames">A list of images to invert.</param>
