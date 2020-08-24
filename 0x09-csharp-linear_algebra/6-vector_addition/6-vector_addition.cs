64df719461511a0c902dfc8e5e2f1af5f5e0b1a7
justinmajetich <justinmajetich@gmail.com>
2020-08-24 19:13:02 -0400

    Write method to add 2D and 3D vectors

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+/// <summary>
+/// Provides a suite of linear algebra helper methods.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Add two 2D or 3D vectors.
+    /// </summary>
+    /// <param name="vector1">Vector to add to.</param>
+    /// <param name="vector2">Vector to be added to.</param>
+    /// <returns></returns>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if ((vector1.Length == 2 || vector1.Length == 3) && vector1.Length == vector2.Length)
+        {
+            for (int i = 0; i < vector1.Length; i++)
+                vector1[i] += vector2[i];
+            return vector1;
+        }
+        return new double[] {-1};
+    }
+}
