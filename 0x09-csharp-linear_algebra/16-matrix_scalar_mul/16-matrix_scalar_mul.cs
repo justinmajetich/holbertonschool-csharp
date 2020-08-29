3652f4be6cae627a68eb062a1cfa5d176a8c466f
justinmajetich <justinmajetich@gmail.com>
2020-08-28 20:02:01 -0400

    Add check for row length

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -10,7 +10,8 @@ class MatrixMath
     /// </summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if ((matrix.GetLength(0) == 2 || matrix.GetLength(0) == 3))
+        if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) || 
+            (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3))
         {
             for (int y = 0; y < matrix.GetLength(0); y++)
             {
