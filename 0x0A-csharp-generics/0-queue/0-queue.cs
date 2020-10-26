2c74c62f040904cac4feb89d6a20e9835f15fdcc
Justin Majetich <justinmajetich@gmail.com>
2020-10-26 13:03:08 -0400

    Add XML documentation

--- 0x0A-csharp-generics/0-queue/0-queue.cs
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -2,6 +2,10 @@
 
 
 class Queue<T> {
+    /// <summary>
+    /// Gets the type of the instance.
+    /// </summary>
+    /// <returns>Returns the instance type.</returns>
     public Type CheckType() {
         return typeof(T);
     }
