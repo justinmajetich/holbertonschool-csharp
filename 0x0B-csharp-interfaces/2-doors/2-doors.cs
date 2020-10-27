a699ec9d62da7095732ee5524f81acb98b10356f
justinmajetich <justinmajetich@gmail.com>
2020-10-27 15:23:01 -0400

    Implement Door class with IInteractable

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+class Door : Base, IInteractive {
+
+    public Door(string _name = "Door") {
+        name = _name;
+    }
+
+    public void Interact() {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+
+}
+
+/// <summary>
+/// A base class.
+/// </summary>
+abstract class Base {
+
+    public string name { get; set; }
+
+    /// <summary>
+    /// Convert the object to a string.
+    /// </summary>
+    public override string ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
+
+/// <summary>
+/// Interface defining an interactable object.
+/// </summary>
+public interface IInteractive {
+
+    /// <summary>
+    /// Interact with this object.
+    /// </summary>
+    void Interact();
+
+}
+
+/// <summary>
+/// Interface defining a breakable object.
+/// </summary>
+public interface IBreakable {
+
+    /// <summary>
+    /// The durability of the breakable object.
+    /// </summary>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Break this object.
+    /// </summary>
+    void Break();
+
+}
+
+/// <summary>
+/// Interface defining a collectable object.
+/// </summary>
+public interface ICollectable {
+
+    /// <summary>
+    /// Denotes whether the object has been collected.
+    /// </summary>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collect this object.
+    /// </summary>
+    void Collect();
+
+}
