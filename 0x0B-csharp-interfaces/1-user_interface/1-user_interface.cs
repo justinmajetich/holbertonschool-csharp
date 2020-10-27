ecd6ca19e4f3eacfa3042a87d5c1c086fd727870
justinmajetich <justinmajetich@gmail.com>
2020-10-27 15:09:29 -0400

    Remove Base as super class of TestObject

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -17,11 +17,10 @@ abstract class Base {
 
 }
 
-class TestObject : Base, IInteractive, IBreakable, ICollectable {
+class TestObject : IInteractive, IBreakable, ICollectable {
 
     public int durability { get; set; }
     public bool isCollected { get; set; }
-    private string name { get; set; }
 
     public void Interact() {
         throw new NotImplementedException();
