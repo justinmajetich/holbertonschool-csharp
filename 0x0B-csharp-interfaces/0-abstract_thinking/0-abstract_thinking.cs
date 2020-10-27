8a26ddca327dd018b13658ea4ffa94e97e7ff3a9
justinmajetich <justinmajetich@gmail.com>
2020-10-27 12:49:20 -0400

    Write abstract Base class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+abstract class Base {
+
+    public string name { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
