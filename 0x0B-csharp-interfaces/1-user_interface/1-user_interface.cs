74ad7fa7eb247b01ec9292f66de702ab6ca6de4c
justinmajetich <justinmajetich@gmail.com>
2020-10-27 13:36:23 -0400

    Create three interaces

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+abstract class Base {
+
+    public string name { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() {
+        return $"{name} is a {this.GetType()}";
+    }
+
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable {
+
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact() {
+        throw new NotImplementedException();
+    }
+
+    public void Break() {
+        throw new NotImplementedException();
+    }
+
+    public void Collect() {
+        throw new NotImplementedException();
+    }
+
+}
+
+/// <summary>
+/// 
+/// </summary>
+public interface IInteractive {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Interact();
+
+}
+
+/// <summary>
+/// 
+/// </summary>
+public interface IBreakable {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <value></value>
+    int durability { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Break();
+
+}
+
+/// <summary>
+/// 
+/// </summary>
+public interface ICollectable {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <value></value>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Collect();
+
+}
