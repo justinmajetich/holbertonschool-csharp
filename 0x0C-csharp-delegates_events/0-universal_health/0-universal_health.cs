8c802c7bee2d95fd06e1d1cfd76ac2e31c3a37bd
Justin Majetich <justinmajetich@gmail.com>
2020-10-28 15:35:44 -0400

    Create Player class

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/// <summary>
+/// Represents a player.
+/// </summary>
+public class Player
+{
+    string name { get; set; }
+    float maxHp { get; set; }
+    float hp { get; set; }
+
+    /// <summary>
+    /// Constructor for player object.
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f) {
+        this.name = name;
+
+        if (maxHp > 0) {
+            this.maxHp = maxHp;
+        } else {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Prints the health of the player.
+    /// </summary>
+    public void PrintHealth() {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+}
